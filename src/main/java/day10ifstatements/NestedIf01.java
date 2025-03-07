ff08b24c59e9ea1f9b27f796cf62c35d3cec22b0
Yahya Erd <117825451+yahyaerd44@users.noreply.github.com>
2025-03-08 02:41:41 +0300

    some changes

--- src/main/java/day10ifstatements/NestedIf01.java
+++ src/main/java/day10ifstatements/NestedIf01.java
@@ -1,5 +1,7 @@
 package day10ifstatements;
 
+import java.util.Scanner;
+
 public class NestedIf01 {//Nested : ic ice gecmis demek.inner icteki .outer dıstaki demek
     public static void main(String[] args) {
         /* Example 1: Kullanicidan 3 tane sayi aliniz.
@@ -13,6 +15,25 @@ public class NestedIf01 {//Nested : ic ice gecmis demek.inner icteki .outer dıs
         b+c>a>|b-c|
         a=b=c ise eskenar ucgen
         */
+        Scanner input = new Scanner(System.in);
+        System.out.println("Ucgenin kenarlari icin 3 tane uzunluk giriniz..");
 
+      double a = Math.abs(input.nextDouble());
+      double b = Math.abs(input.nextDouble());
+      double c = Math.abs(input.nextDouble());
+
+      boolean ucgenmi =  (a+b>c && c>Math.abs(a-b)) &&  // a+b>c>|a-b|)==>matematikte bu sekilde yazılır kod icine yazdıgımız java dilinde.
+                         (a+c>b && b>Math.abs(a-c)) &&  // a+c>b>|a-c|
+                         (b+c>b && a>Math.abs(b-c));    // b+c>a>|b-c|
+
+      if (ucgenmi) {
+          if (a==b && b==c && a==c){
+              System.out.println("ucgendir hem de eskenar ucgendir");
+          }else {
+              System.out.println("Ucgendir ama eskenar degil..");
+          }
+      }else {
+          System.out.println("Bu bir ucgen degildir");
+      }
     }
 }
