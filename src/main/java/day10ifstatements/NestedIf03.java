f993e7c30d936333312f46f5fa8907b295a663b5
Yahya Erd <117825451+yahyaerd44@users.noreply.github.com>
2025-03-10 02:51:27 +0300

    some changes

--- src/main/java/day10ifstatements/NestedIf03.java
+++ src/main/java/day10ifstatements/NestedIf03.java
@@ -13,17 +13,22 @@ public class NestedIf03 {
         "gecersiz password" yazdirin
         zehirli==>Gecerli
      */
+        //Note : Eger bir methodu tekrar tekrar kullanmak istiyorsaniz onu bir variable a atayarak kullanmak daha makuldur
+        //ornek char firstChar = pwd.charAt(0); gibi
         Scanner input = new Scanner(System.in);
         System.out.println("Passwordunuzu giriniz..");
         String pwd = input.nextLine();
-        if (pwd.charAt(0)>='A' && pwd.charAt(0)<='Z') {//Password'un ilk harfi buyuk harf ise
-            if (pwd.charAt(0)=='A') {
+
+        char firstChar = pwd.charAt(0);
+
+        if (firstChar>='A' && firstChar<='Z') {//Password'un ilk harfi buyuk harf ise
+            if (firstChar=='A') {
                 System.out.println("Gecerli password");
             } else {
                 System.out.println("Gecersiz password cunku harf buyuk ama A degil");
             }
-        } else if (pwd.charAt(0)>='a' && pwd.charAt(0)<='z') {// Passwordun ilk harfi kucuk harf ise
-            if (pwd.charAt(0)=='z'){
+        } else if (firstChar>='a' && firstChar<='z') {// Passwordun ilk harfi kucuk harf ise
+            if (firstChar=='z'){
                 System.out.println("Gecerli password");
             }else{
                 System.out.println("Gecersiz password cunku kucuk harf ama z degil");
---
+++ src/main/java/day11nestedifternaryincrementdecrement/NestedIf01.java
@@ -0,0 +1,42 @@
+package day11nestedifternaryincrementdecrement;
+
+import java.util.Scanner;
+
+public class NestedIf01 {
+    public static void main(String[] args) {
+        /*
+        Kullanicidan 0'dan kucuk 120'den buyuk deger giremeyecek sekilde datalari aldiktan sonra
+
+        Eger calisan kadin ise ;
+        60 yasindan buyukse "Emekli olabilir" yazdirin
+
+        Eger calisan erkek ise;
+        65 yasindan buyukse "Emekli olabilir" yazdiran kodu olusturunuz
+       */
+        Scanner input = new Scanner(System.in);
+        System.out.println("Lutfen cinsiyetinizi giriniz..");
+        String gender = input.next();
+
+        System.out.println("Lutfen yasinizi giriniz..");
+        int age = input.nextInt();
+        if (gender.equalsIgnoreCase("Kadin")) {
+
+            if (age > 60) {
+                System.out.println("Emekli olabilir");
+            } else {
+                System.out.println("Calismali...");
+            }
+        } else if (gender.equalsIgnoreCase("Erkek")) {
+            if (age > 65) {
+                System.out.println("Emekli olabilir");
+            } else {
+                System.out.println("Kesinlikle calismali");
+            }
+        } else {
+            System.out.println("Tanımlı degil...");
+        }
+    }
+}
+
+
+
